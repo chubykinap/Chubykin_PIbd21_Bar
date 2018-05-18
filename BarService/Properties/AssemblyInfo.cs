7af6c13c1caaa45ded9a28e7df0a35bfe6493571
chubykinap <coperi98@gmail.com>
2018-05-18 16:54:02 +0400

    Лабораторная №9

--- BarService/Properties/AssemblyInfo.cs
+++ BarService/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ClassLibrary1")]
+[assembly: AssemblyTitle("BarService")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ClassLibrary1")]
+[assembly: AssemblyProduct("BarService")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("bcae429a-6925-4def-919d-aca0000275b5")]
+[assembly: Guid("57084db3-ee23-4cb2-97af-a0fa54f58b6a")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
