7d03637cc6f81d0cb828272099678532bbb9629d
chubykinap <coperi98@gmail.com>
2018-05-22 14:26:34 +0400

    Лабораторная 12

---
+++ BarService/Interfaces/IReportService.cs
@@ -0,0 +1,19 @@
+﻿using BarService.BindingModels;
+using BarService.ViewModel;
+using System.Collections.Generic;
+
+namespace BarService.Interfaces
+{
+    public interface IReportService
+    {
+        void SaveCocktailPrice(ReportBindModel model);
+
+        List<StorageLoadViewModel> GetStoragesLoad();
+
+        void SaveStoragesLoad(ReportBindModel model);
+
+        List<CustomerOrderViewModel> GetCustomerOrders(ReportBindModel model);
+
+        void SaveCustomerOrders(ReportBindModel model);
+    }
+}
---
+++ BarService/ViewModel/CustomerOrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace BarService.ViewModel
+{
+    public class CustomerOrderViewModel
+    {
+        public string CustomerName { get; set; }
+
+        public string DateCreate { get; set; }
+
+        public string CocktailName { get; set; }
+
+        public int Count { get; set; }
+
+        public decimal Sum { get; set; }
+
+        public string Status { get; set; }
+    }
+}
